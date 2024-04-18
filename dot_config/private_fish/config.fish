20588d5573492e2d18e03d50bb52dc45f1002090
letieu <letieu8@gmail.com>
2024-04-18 23:39:24 +0700

    feat(fish): cleanup the config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -25,18 +25,7 @@ alias ghe="gh copilot explain"
 alias icat="wezterm imgcat"
 alias kv="NVIM_APPNAME=nvim-kickstart nvim"
 
-if status is-interactive
-end
-
 fish_vi_key_bindings
-
-set macos_theme (defaults read -g AppleInterfaceStyle 2>/dev/null)
-
-if test "$macos_theme" = "Dark"
-    fish_config theme choose "Catppuccin Mocha"
-else
-    fish_config theme choose "Catppuccin Latte"
-end
 
 starship init fish | source
 fzf --fish | source
--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -16,9 +16,17 @@ config.initial_rows = 45
 config.initial_cols = 180
 
 config.use_fancy_tab_bar = false
+-- use tab at the bottom
+config.tab_bar_at_bottom = true
 
 config.keys = keys
 config.hyperlink_rules = hyperlink_rules
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
 
 local function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
