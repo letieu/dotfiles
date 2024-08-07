b702d76cb739bed1bae38486b7c1ae1eda0c83f0
letieu <letieu8@gmail.com>
2024-08-07 15:18:06 +0700

    feat: add gcloud

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -5,6 +5,12 @@ set -x EDITOR "nvim"
 set -x ANDROID_HOME "/opt/android-sdk"
 set -x JAVA_HOME "/usr/lib/jvm/java-17-openjdk"
 
+set -x CLOUDSDK_ROOT_DIR /opt/google-cloud-cli
+set -x CLOUDSDK_PYTHON /usr/bin/python
+set -x CLOUDSDK_PYTHON_ARGS '-S -W ignore'
+set -x PATH $CLOUDSDK_ROOT_DIR/bin $PATH
+set -x GOOGLE_CLOUD_SDK_HOME $CLOUDSDK_ROOT_DIR
+
 # PATH
 set PATH ~/.cargo/bin $PATH
 set PATH ~/go/bin $PATH
