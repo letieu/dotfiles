5ae606d2372f9de38e0c122ca6d9ddd0489112b0
letieu <letieu8@gmail.com>
2024-12-24 22:01:51 +0700

    chore: move install pacakge script to a file

--- README.md
+++ README.md
@@ -1,50 +1,13 @@
 
-## Install package
-```bash
-# list package installed
-# history | grep 'sudo pacman -S'
-sudo pacman -S fastfetch
-sudo pacman -S hyprland
-sudo pacman -S --needed base-devel git
-sudo pacman -S htop
-sudo pacman -S auto-cpufreq
-sudo pacman -S neovim
-sudo pacman -S kitty
-sudo pacman -S firefox
-sudo pacman -S bemenu
-sudo pacman -S zoxide
-sudo pacman -S tldr
-sudo pacman -S j4-dmenu-desktop
-sudo pacman -S pipewire
-sudo pacman -S pipewire-audio
-sudo pacman -S pipewire-alsa
-sudo pacman -S wireplumber
-sudo pacman -S pipewire-pulse
-sudo pacman -S sof-firmware
-sudo pacman -S brightnessctl
-sudo pacman -S skim
-sudo pacman -S ranger
-sudo pacman -S xdg-open
-sudo pacman -S xdg-utils
-sudo pacman -S fcitx5 fcitx5-qt fcitx5-gtk fcitx5-unikey fcitx5-config-qt
-sudo pacman -S xdg-desktop-portal-wlr
-sudo pacman -S openssh
-sudo pacman -S swappy
-sudo pacman -S mako libnotify
-
-# history search --contains "yay -S"
-yay -S hyprland-git
-yay -S chezmoi
-yay -S kitty-git
-yay -S auto-cpufreq
-yay -S ttf-jetbrains-mono-nerd
-yay -S grimblast
-```
-
 ## Apply config
 ```bash
+# Install packages
+bash init_package.sh
+
+# Apply config
 chezmoi init --apply letieu
 ```
 
 ## Neovim
 - Neovim [config](https://github.com/letieu/nvim-config)
+
---
+++ init_package.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# history | grep 'sudo pacman -S'
+sudo pacman -S fastfetch
+sudo pacman -S less
+sudo pacman -S hyprland
+sudo pacman -S --needed base-devel git
+sudo pacman -S htop
+sudo pacman -S auto-cpufreq
+sudo pacman -S neovim
+sudo pacman -S firefox
+sudo pacman -S bemenu
+sudo pacman -S zoxide
+sudo pacman -S tldr
+sudo pacman -S j4-dmenu-desktop
+sudo pacman -S pipewire
+sudo pacman -S pipewire-audio
+sudo pacman -S pipewire-alsa
+sudo pacman -S wireplumber
+sudo pacman -S pipewire-pulse
+sudo pacman -S sof-firmware
+sudo pacman -S brightnessctl
+sudo pacman -S skim
+sudo pacman -S ranger
+sudo pacman -S xdg-open
+sudo pacman -S xdg-utils
+sudo pacman -S fcitx5 fcitx5-qt fcitx5-gtk fcitx5-unikey fcitx5-config-qt
+sudo pacman -S xdg-desktop-portal-wlr
+sudo pacman -S openssh
+sudo pacman -S swappy
+sudo pacman -S mako libnotify
+sudo pacman -S acpi
+sudo pacman -S inetutils
+sudo pacman -S docker
+sudo pacman -S docker-compose
+sudo pacman -S fcitx5 fcitx5-qt fcitx5-gtk fcitx5-unikey fcitx5-config-qt
+sudo pacman -S wl-clipboard
+sudo pacman -S neovim
+
+# history search --contains "yay -S"
+yay -S chezmoi
+yay -S hyprland-git
+yay -S kitty-git
+yay -S auto-cpufreq
+yay -S ttf-jetbrains-mono-nerd
+yay -S grimblast
+yay -S python-pyclip
+yay -S libpg
+yay -S ripgrep
+yay -S fd
+yay -S skim
+yay -S bemenu
