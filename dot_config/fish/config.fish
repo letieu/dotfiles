e0eb5b6561b8a5e8d7aa8b8722fd1055b8d3a547
letieu <letieu8@gmail.com>
2025-06-14 17:12:47 +0700

    feat: add waybar config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -10,18 +10,21 @@ set -x CLOUDSDK_PYTHON_ARGS '-S -W ignore'
 set -x PATH $CLOUDSDK_ROOT_DIR/bin $PATH
 set -x GOOGLE_CLOUD_SDK_HOME $CLOUDSDK_ROOT_DIR
 
-# set -x JAVA_HOME /opt/android-studio/jbr/
-
 # PATH
-fish_add_path ~/.cargo/bin
-fish_add_path ~/go/bin
-fish_add_path ~/.local/bin
-fish_add_path ~/scripts
-fish_add_path ~/.local/share/nvm/v22.12.0/bin
-fish_add_path /nix/var/nix/profiles/default/bin
-fish_add_path /opt/android-studio/android-studio/bin/
-fish_add_path ~/Android/Sdk/platform-tools
-fish_add_path ~/node_modules/.bin
+for dir in \
+    ~/.cargo/bin \
+    ~/go/bin \
+    ~/.local/bin \
+    ~/scripts \
+    ~/.local/share/nvm/v22.12.0/bin \
+    /nix/var/nix/profiles/default/bin \
+    /opt/android-studio/android-studio/bin/ \
+    ~/Android/Sdk/platform-tools \
+    ~/.yarn/bin
+    if test -d $dir
+        fish_add_path $dir
+    end
+end
 
 # Aliases
 alias h="Hyprland"
@@ -35,23 +38,3 @@ fish_vi_key_bindings
 zoxide init fish | source
 
 fish_config theme choose "Catppuccin Latte"
-
-__check_nvm
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/opt/google-cloud-cli/path.fish.inc' ]; . '/opt/google-cloud-cli/path.fish.inc'; end
-
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/tieu/.ghcup/bin $PATH # ghcup-env
-
-# pnpm
-set -gx PNPM_HOME "/home/tieu/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -Ux PATH $PYENV_ROOT/bin $PATH
-status --is-interactive; and source (pyenv init -|psub)
-
-direnv hook fish | source
--- dot_config/hypr/scripts/executable_volume.sh
+++ dot_config/hypr/scripts/executable_volume.sh
@@ -2,32 +2,23 @@
 
 send_vol_notification() {
     hyprctl dismissnotify # dismiss all notifications
-    local volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+%' | head -1)
+    local volume=$(wpctl get-volume @DEFAULT_SINK@)
     hyprctl notify -1 2000 "rgb(ff0000)" " 󰕾 : ${volume} "
-}
-
-send_mute_notification() {
-    local muted=$(pactl get-sink-mute @DEFAULT_SINK@ | grep -oP 'yes|no' | head -1)
-    if [ "$muted" == "yes" ]; then
-        hyprctl notify -1 2000 "rgb(ff0000)" "  󰖁 Muted  "  # Nerd Font icon for mute
-    else
-        hyprctl notify -1 2000 "rgb(00ff00)" "  󰕾 On  "  # Nerd Font icon for unmute
-    fi
 }
 
 # Change the volume
 case $1 in
     up)
-        pactl set-sink-volume @DEFAULT_SINK@ +5%
-        send_vol_notification
+        wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+        # send_vol_notification
         ;;
     down)
-        pactl set-sink-volume @DEFAULT_SINK@ -5%
-        send_vol_notification
+        wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+        # send_vol_notification
         ;;
     mute)
-        pactl set-sink-mute @DEFAULT_SINK@ toggle
-        send_mute_notification
+        wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+        # send_vol_notification
         ;;
     *)
         echo "Usage: $0 {up|down|mute}"
